b23264dbb7cc883d3ebc6bf908734204044ac625
chenwenwen1 <k1623103@kcl.ac.uk>
2017-02-19 19:29:52 +0000

    Add files via upload

---
+++ 03_Windows application/client/client/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public String text_to_send;
+        public string recieve;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient();
+            IPEndPoint IP_End = new IPEndPoint(IPAddress.Parse(ipbox.Text), int.Parse(portbox.Text));
+
+            try
+            {
+                client.Connect(IP_End);
+                if(client.Connected)
+                {
+                    textBox2.AppendText("connected to server" + "\n");
+                    STW = new StreamWriter(client.GetStream());
+                    STR = new StreamReader(client.GetStream());
+                    STW.AutoFlush = true;
+
+                    backgroundWorker1.RunWorkerAsync();
+                    backgroundWorker2.WorkerSupportsCancellation = true;
+
+
+                }
+            }
+            catch(Exception x)
+            {
+                MessageBox.Show(x.Message.ToString());
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != null)
+            {
+                text_to_send = textBox1.Text;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            textBox1.Text = "";
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while(client.Connected)
+            {
+                try
+                {
+                    recieve = STR.ReadLine();
+                    this.textBox2.Invoke(new MethodInvoker(delegate () { textBox2.AppendText("you : " + recieve + "\n"); }));
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(x.Message.ToString());
+                }
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (client.Connected)
+            {
+                STW.WriteLine(text_to_send);
+                this.textBox2.Invoke(new MethodInvoker(delegate () { textBox2.AppendText("me : " + text_to_send + "\n"); }));
+            }
+            else
+            {
+                MessageBox.Show("send failed!");
+            }
+                backgroundWorker2.CancelAsync();
+        }
+    }
+}
