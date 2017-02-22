0cc44fe43679ba5c253812e431ae6e1bdbd5f28a
chenwenwen1 <k1623103@kcl.ac.uk>
2017-02-22 15:55:51 +0000

    chat list

---
+++ 03_Windows application/chat list/chat list/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chat_list
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chatbox1.clear();
+        }
+    }
+}
---
+++ 03_Windows application/chat list/chat list/chatbox.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chat_list
+{
+    public partial class chatbox : UserControl
+    {
+        chatmesg bbl_old = new chatmesg();
+        public chatbox()
+        {
+            //if (!this.DesignMode)
+            //{
+               InitializeComponent();
+            // }
+
+            bbl_old.Top = 0 - bbl_old.Height + 10;
+        }
+
+        int curtop = 10;
+        int lst = 0;
+        
+
+
+        public void addInMessage(string message, string time)
+        { chatmesg bbl = new chat_list.chatmesg(message, time, msgtype.In);
+            bbl.Location = bubble1.Location;
+            bbl.Size = bubble1.Size;
+            bbl.Anchor = bubble1.Anchor;
+            //if (bbl.Location != bubble1.Location)
+            
+                bbl.Top = bbl_old.Bottom + 10;
+                //curtop = bbl.Bottom + 10;
+                panel1.Controls.Add(bbl);
+                panel1.VerticalScroll.Value = panel1.VerticalScroll.Maximum;
+
+                bbl_old = bbl;// safe the last added object
+            
+            
+        }
+
+        public void addOutMessage(string message, string time)
+        {
+            chatmesg bbl = new chat_list.chatmesg(message, time, msgtype.Out);
+            bbl.Location = bubble1.Location;
+            bbl.Left += 20;
+            bbl.Size = bubble1.Size;
+            bbl.Anchor = bubble1.Anchor;
+           
+                bbl.Top = bbl_old.Bottom + 10;
+                //curtop = bbl.Bottom + 10;
+                panel1.Controls.Add(bbl);
+                panel1.VerticalScroll.Value = panel1.VerticalScroll.Maximum;
+                bbl_old = bbl;
+            // safe the last added object
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            addInMessage("Hello world", "00:00");
+            addOutMessage("Hello world to you too", "00:00");
+            panel1.VerticalScroll.Value = panel1.VerticalScroll.Maximum;
+        }
+        public void clear()
+        {
+            bbl_old = new chatmesg();
+
+        }
+    }
+}
